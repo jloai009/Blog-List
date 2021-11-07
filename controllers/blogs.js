fe7ea66320866e7f68d565b594430c86fb3b1ed8
Jose <jloai009@fiu.edu>
2021-11-07 06:45:15 -0500

    Del console.logs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,30 +2,25 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (request, response, next) => {
-  console.log('Incoming GET')
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
     })
     .catch(error => {
-      console.log(error)
       next(error)
     })
 })
 
 blogsRouter.post('/', (request, response, next) => {
-  console.log('Incoming POST', request.body)
   const blog = new Blog(request.body)
 
   blog
     .save()
     .then(result => {
-      console.log('Response from MongoDB', result)
       response.status(201).json(result)
     })
     .catch(error => {
-      console.log(error)
       next(error)
     })
 })
