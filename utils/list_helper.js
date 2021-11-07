4238ff8df1d2c3ec900c00809d4747629bfb4f46
Jose <jloai009@fiu.edu>
2021-11-07 10:15:14 -0500

    Add function: mostLikes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,3 @@
-const dummy = (blogs) => {
-  return 1
-}
-
 const totalLikes = (blogs) => {
   let totalLikes = 0
 
@@ -51,9 +47,38 @@ const mostBlogs = (blogs) => {
   return ret
 }
 
+const mostLikes = (blogs) => {
+  const ret = {
+    author: null,
+    likes: 0
+  }
+
+  if (!blogs || blogs.length === 0) {
+    return ret
+  }
+
+  const map = new Map()
+  for (const blog of blogs) {
+    if (map.has(blog.author)) {
+      map.set(blog.author, map.get(blog.author) + blog.likes)
+    } else {
+      map.set(blog.author, blog.likes)
+    }
+  }
+
+  for (const [author, likes] of map.entries()) {
+    if (likes > ret.likes) {
+      ret.likes = likes
+      ret.author = author
+    }
+  }
+
+  return ret
+}
+
 module.exports = {
-  dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
