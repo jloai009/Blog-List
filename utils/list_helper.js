30da86b0fbddfd4c7890f1f111ed88e29465e070
Jose <jloai009@fiu.edu>
2021-11-07 07:55:43 -0500

    Added totalLikes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let totalLikes = 0
+
+  for (const blog of blogs) {
+    totalLikes += blog.likes
+  }
+
+  return totalLikes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
