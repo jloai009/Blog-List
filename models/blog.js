8939adca8469a43b524438c3986bafa91ee179f5
Jose <jloai009@fiu.edu>
2021-11-07 06:25:09 -0500

    Move MongoDB connection to app.js

--- models/blog.js
+++ models/blog.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-const MONGODB_URI = require('../utils/config').MONGODB_URI
-
-mongoose.connect(MONGODB_URI)
 
 const blogSchema = new mongoose.Schema({
   title: String,
