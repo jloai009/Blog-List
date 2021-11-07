7c9895c6b32c07fb47ea17a522f62eea7a0803f3
Jose <jloai009@fiu.edu>
2021-11-07 02:01:30 -0500

    PORT = env.PORT

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
+
+console.log(MONGODB_URI, PORT)
 
 module.exports = {
   MONGODB_URI,
