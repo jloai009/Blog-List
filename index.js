bc89b3e94ef1fbf484da3751802fc5fa823dcda1
Jose <jloai009@fiu.edu>
2021-11-07 06:24:19 -0500

    Routing moved to controllers/blogs

--- index.js
+++ index.js
@@ -1,42 +1,10 @@
-// const http = require('http')
-const express = require('express')
-const cors = require('cors')
-const Blog = require('./models/blog')
+const app = require('./app')
+const http = require('http')
 const PORT = require('./utils/config').PORT
-const app = express()
+const logger = require('./utils/logger')
 
-app.use(cors())
-app.use(express.json())
+const server = http.createServer(app)
 
-app.get('/api/blogs', (request, response, next) => {
-  console.log('Incoming get')
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    .catch(error => {
-      console.log(error)
-      next(error)
-    })
-})
-
-app.post('/api/blogs', (request, response, next) => {
-  console.log('POST', request.body)
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      console.log('Response from MongoDB', result)
-      response.status(201).json(result)
-    })
-    .catch(error => {
-      console.log(error)
-      next(error)
-    })
-})
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
 })
