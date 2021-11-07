a8ba8424759699db4104f8cc3742159ccb5b814a
Jose <jloai009@fiu.edu>
2021-11-07 03:18:51 -0500

    Add basic error handling

--- index.js
+++ index.js
@@ -7,26 +7,38 @@ const { MONGODB_URI, PORT } = require('./utils/config')
 const app = express()
 
 const mongoUrl = MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose
+  .connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
+app.get('/api/blogs', (request, response, next) => {
+  console.log('Incoming get')
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
     })
+    .catch(error => {
+      console.log(error)
+      next(error)
+    })
 })
 
-app.post('/api/blogs', (request, response) => {
+app.post('/api/blogs', (request, response, next) => {
+  console.log('POST', request.body)
   const blog = new Blog(request.body)
 
   blog
     .save()
     .then(result => {
+      console.log('Response from MongoDB', result)
       response.status(201).json(result)
+    })
+    .catch(error => {
+      console.log(error)
+      next(error)
     })
 })
 
