0ee652bc0907d78401a03ec89621f295ec4462f3
Jose <jloai009@fiu.edu>
2021-11-07 09:54:44 -0500

    Add function: mostBlogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -22,8 +22,38 @@ const favoriteBlog = (blogs) => {
   return favoriteBlog
 }
 
+const mostBlogs = (blogs) => {
+  const ret = {
+    author: null,
+    blogs: 0
+  }
+
+  if (!blogs || blogs.length === 0) {
+    return ret
+  }
+
+  const map = new Map()
+  for (const blog of blogs) {
+    if (map.has(blog.author)) {
+      map.set(blog.author, map.get(blog.author) + 1)
+    } else {
+      map.set(blog.author, 1)
+    }
+  }
+
+  for (const [author, blogs] of map.entries()) {
+    if (blogs > ret.blogs) {
+      ret.blogs = blogs
+      ret.author = author
+    }
+  }
+
+  return ret
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
