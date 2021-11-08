41702f5e3966c8ac17efd63ebf6f7818043c45a9
Jose <jloai009@fiu.edu>
2021-11-08 10:11:45 -0500

    Add tests for GET route

---
+++ tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blogObjects = helper.blogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(helper.blogs.length)
+})
+
+afterAll(() => {
+  console.log('Closing MONGODB Connection')
+  mongoose.connection.close()
+})
