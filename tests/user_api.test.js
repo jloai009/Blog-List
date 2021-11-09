ccd668a2d0ba8a92adb475de367036d84927f3aa
Jose <jloai009@fiu.edu>
2021-11-09 12:38:01 -0500

    Add tests for bad usernames

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -61,6 +61,53 @@ describe('when there is initially one user in db', () => {
       expect(usersAtEnd).toHaveLength(usersAtStart.length)
     }
   )
+
+  test(
+    'creation fails with proper statuscode and message if username not provided',
+    async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        name: 'Superuser',
+        password: 'salainen'
+      }
+
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      expect(result.body.error).toContain('Path `username` is required')
+
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    }
+  )
+
+  test(
+    'creation fails with proper statuscode and message if username is less than 3 characters',
+    async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'ro',
+        name: 'Superuser',
+        password: 'salainen'
+      }
+
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+      expect(result.body.error).toContain('is shorter than the minimum allowed length (3)')
+
+      const usersAtEnd = await helper.usersInDb()
+      expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    }
+  )
 })
 
 afterAll(() => {
