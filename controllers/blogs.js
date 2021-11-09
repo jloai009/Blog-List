6762701a59fefdcd6d962b323073410c04794881
Jose <jloai009@fiu.edu>
2021-11-08 19:00:59 -0500

    Add PUT handler

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -17,4 +17,17 @@ blogsRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+  const blog = {
+    title: request.body.title,
+    url: request.body.url,
+    likes: request.body.likes
+  }
+
+  const updatedBlog =
+    await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+
+  response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
