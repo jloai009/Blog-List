1ff360c016a6a8d86f6b6b66f3f5afbe6c71a8eb
Jose <jloai009@fiu.edu>
2021-11-09 14:17:54 -0500

    post blog now requires token auth

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,39 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response, next) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+
+  return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
   const blog = new Blog(request.body)
-  const result = await blog.save()
+
+  const user = await User.findById(decodedToken.id)
+  blog.user = user._id.toString()
+  user.blogs = user.blogs.concat(blog._id)
+  user.save()
+
+  let result = await blog.save()
+  result = await result.populate('user', { username: 1, name: 1 })
+
   response.status(201).json(result)
 })
 
