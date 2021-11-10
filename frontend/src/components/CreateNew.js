aa5e651e7fe0032b283c3e5495d29b69f4a09f4c
Jose <jloai009@fiu.edu>
2021-11-10 11:01:52 -0500

    Added CreateNew component

---
+++ frontend/src/components/CreateNew.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const CreateNew = (props) => (
+  <div>
+    <h3>Create New</h3>
+    <form onSubmit={props.handleCreateNew}>
+      <div>
+        <label>Title:</label>
+        <input
+          type="text"
+          value={props.title}
+          name="Title"
+          onChange={({ target }) => { props.setTitle(target.value) }}
+        />
+      </div>
+      <div>
+        <label>Author:</label>
+        <input
+          type="text"
+          value={props.author}
+          name="Author"
+          onChange={({ target }) => { props.setAuthor(target.value) }}
+        />
+      </div>
+      <div>
+        <label>URL:</label>
+        <input
+          type="text"
+          value={props.url}
+          name="URL"
+          onChange={({ target }) => { props.setUrl(target.value) }}
+        />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  </div>
+)
+
+export default CreateNew
