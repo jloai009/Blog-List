116605c88b95a27690e955b4b7266c38ce572a29
Jose <jloai009@fiu.edu>
2021-11-10 12:31:16 -0500

    Update LoginForm component

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,34 +1,41 @@
 import React from "react"
+import Notification from "./Notification"
 
-const LoginForm = ( props ) => (
-  <div>
-    <h2>Log in to Blog-List</h2>
-    <form onSubmit={props.handleLogin}>
-      <div>
-        <label>Username: </label>
-        <input
-          type="text"
-          value={props.username}
-          name="Username"
-          onChange={({ target }) => {
-            props.setUsername(target.value)
-          }}
-        />
-      </div>
-      <div>
-        <label>Password: </label>
-        <input
-          type="password"
-          value={props.password}
-          name="Password"
-          onChange={({ target }) => {
-            props.setPassword(target.value)
-          }}
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>
-  </div>
-)
+const LoginForm = (props) => {
+
+  const notificationProps = { notification: props.notification, errorOcurred: props.errorOcurred }
+
+  return (
+    <div>
+      <h2>Log in to Blog-List</h2>
+      <Notification {...notificationProps} />
+      <form onSubmit={props.handleLogin}>
+        <div>
+          <label>Username: </label>
+          <input
+            type="text"
+            value={props.username}
+            name="Username"
+            onChange={({ target }) => {
+              props.setUsername(target.value)
+            }}
+          />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input
+            type="password"
+            value={props.password}
+            name="Password"
+            onChange={({ target }) => {
+              props.setPassword(target.value)
+            }}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
 
 export default LoginForm
