74b87cd13ef5e678eb23d2f5dd17893a0b2ec968
Jose <jloai009@fiu.edu>
2021-11-10 08:37:08 -0500

    Add login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +15,92 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      window.localStorage.setItem(
+        'loggedBloglistUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      console.log(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.error('Wrong credentials')
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>Log in to Blog-List</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Username: </label>
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => {
+              setUsername(target.value)
+              console.log(target.value)
+            }}
+          />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => {
+              setPassword(target.value)
+              console.log(target.value)
+            }}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBloglistUser')
+    setUser(null)
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user
+        ?
+        <div>
+          <h2>Blog-List</h2>
+          <div>
+            <p>{user.name} logged in <button onClick={handleLogout}>Logout</button></p>
+          </div>
+          <div>
+            {blogs.map(blog =>
+              <Blog key={blog.id} blog={blog} />
+            )}
+          </div>
+        </div>
+        :
+        loginForm()
+      }
     </div>
   )
 }
