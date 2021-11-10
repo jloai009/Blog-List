ae4e3c6c6ceda527da51762197e0d17949596925
Jose <jloai009@fiu.edu>
2021-11-10 12:30:32 -0500

    Added Notification component

---
+++ frontend/src/components/Notification.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Notification = (props) => {
+  if (props.notification === null) {
+    return null
+  }
+
+  const style = {
+    color: props.errorOcurred ? 'red' : 'green',
+    border: 3,
+    borderStyle: 'solid',
+    borderColor: props.errorOcurred ? 'red' : 'green',
+    borderRadius: 4,
+    backgroundColor: 'silver',
+    padding: 9,
+    fontSize: 20,
+    width: 300
+  }
+
+  return (
+    <p style={style}>{props.notification}</p>
+  )
+}
+
+export default Notification
