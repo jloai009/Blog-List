2cfe58e7502848e38451a59bfd3203f18340faee
Jose <jloai009@fiu.edu>
2021-11-10 12:31:34 -0500

    Update Header Component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,22 @@
 import React from "react"
+import Notification from "./Notification"
 
-const Header = (props) => (
-  <div>
-    <h2>Blog-List</h2>
+const Header = (props) => {
+
+  const notificationProps = { notification: props.notification, errorOcurred: props.errorOcurred }
+
+  return (
     <div>
-      <p>{props.user.name} logged in <button onClick={props.handleLogout}>Logout</button></p>
+      <h2>Blog-List</h2>
+      <Notification {...notificationProps} />
+      <div>
+        <p>
+          {props.user.name} logged in
+          <button onClick={props.handleLogout}>Logout</button>
+        </p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Header
