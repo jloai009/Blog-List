74fef600c320c246b2d2750402c2de9de05087f9
Jose <jloai009@fiu.edu>
2021-11-11 09:12:47 -0500

    Update blogs.js

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -29,29 +30,23 @@ blogsRouter.delete('/:id', async (request, response) => {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
-  const user = request.user
+  let user = request.user
   const blog = await Blog.findById(request.params.id)
 
   if (blog.user.toString() !== user._id.toString()) {
     return response.status(401).json({ error: 'no permission to delete blog' })
   }
 
+  user = await User.findById(user._id.toString())
+  user.blogs = user.blogs.filter(id => id.toString() !== request.params.id)
+
+  await user.save()
   await Blog.findByIdAndDelete(request.params.id)
+
   response.status(204).end()
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  if (!request.user) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-
-  const user = request.user
-  const blog = await Blog.findById(request.params.id)
-
-  if (blog.user.toString() !== user._id.toString()) {
-    return response.status(401).json({ error: 'no permission to delete blog' })
-  }
-
   const updatedBlog =
     await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true })
       .populate('user', { username: 1, name: 1 })
