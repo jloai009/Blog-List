917447c4e42615731c9164995e6af2e93a41cacd
Jose <jloai009@fiu.edu>
2021-11-11 06:35:40 -0500

    Add show info feature

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showInfo, setShowInfo] = useState(false)
+  const [buttonText, setButtonText] = useState('View')
+
+  const toggleShowInfo = () => {
+    setButtonText(showInfo ? 'View' : 'Hide')
+    setShowInfo(!showInfo)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const blogInfo = () => (
+    <div>
+      <div>{blog.url}</div>
+      <div>
+        Likes {blog.likes}&nbsp;
+        <button> Like </button>
+      </div>
+      <div>{blog.user.username}</div>
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        &ensp;
+        <button onClick={toggleShowInfo}> {buttonText} </button>
+      </div>
+      {showInfo ? blogInfo() : null }
+    </div>
+  )
+}
 
 export default Blog
