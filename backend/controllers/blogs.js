7713360f884eca1825956ef921deb48c2c781efb
Jose <jloai009@fiu.edu>
2021-11-11 08:07:13 -0500

    Add authentication needed for put by id

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,7 +1,5 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -43,14 +41,20 @@ blogsRouter.delete('/:id', async (request, response) => {
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  const blog = {
-    title: request.body.title,
-    url: request.body.url,
-    likes: request.body.likes
+  if (!request.user) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = request.user
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog.user.toString() !== user._id.toString()) {
+    return response.status(401).json({ error: 'no permission to delete blog' })
   }
 
   const updatedBlog =
-    await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true })
+      .populate('user', { username: 1, name: 1 })
 
   response.json(updatedBlog)
 })
